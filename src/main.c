fbbc9884e30e279b08e672cbb7aee9e45180c5d5
tirex03 <tirex003@gmail.com>
2021-02-14 15:05:49 +0100

    more changes

--- src/main.c
+++ src/main.c
@@ -11,41 +11,51 @@ double random_norm(){
     return (2 * (int)(randombytes_random() & 1) - 1) * (randombytes_random() / (double)(0xffffffff));
 }
 
-int create_char_texture(SDL_Renderer* renderer, char* filename, SDL_Texture** char_texture){
+int create_font_texture(SDL_Renderer* renderer, char* filename, SDL_Texture** font_texture){
     SDL_Surface* char_surface = SDL_LoadBMP(filename);
     if(char_surface == NULL)
         return -1;
 
-    *char_texture = SDL_CreateTextureFromSurface(renderer, char_surface);
+    *font_texture = SDL_CreateTextureFromSurface(renderer, char_surface);
 
-    if(*char_texture == NULL)
+    if(*font_texture == NULL)
         return -1;
 
     SDL_FreeSurface(char_surface);
     return 0;
 }
 
-void draw_char(SDL_Renderer* renderer, SDL_Texture* char_texture, int charidx, int pos, int off_x, int off_y, int isize){
-    int w, h;
-    SDL_QueryTexture(char_texture, NULL, NULL, &w, &h);
-    SDL_Rect srcrect;
-    srcrect.x = charidx * w / 10;
-    srcrect.y = 0;
-    srcrect.w = w / 10;
-    srcrect.h = h;
-    SDL_Rect dstrect;
-    dstrect.x = off_x + pos * w / 10 / isize;
-    dstrect.y = off_y;
-    dstrect.w = w / 10 / isize;
-    dstrect.h = h / isize;
-    SDL_RenderCopy(renderer, char_texture, &srcrect, &dstrect);
-}
+void print_text(SDL_Renderer* renderer, SDL_Texture* font_texture, size_t font_size, SDL_Texture* canvas, int* font_map, char* text){
+    int char_tex_w, char_tex_h;
+    SDL_QueryTexture(font_texture, NULL, NULL, &char_tex_w, &char_tex_h);
+    int char_w = char_tex_w / font_size;
+    int char_h = char_tex_h;
 
-void draw_chars(SDL_Renderer* renderer, SDL_Texture* char_texture, int* charidxs, int count, int off_x, int off_y, int isize){
-    for(int i = 0; i<count; i++){
-        if(charidxs[i]>=0)
-            draw_char(renderer, char_texture, charidxs[i], i, off_x, off_y, isize);
+    int canvas_w, canvas_h;
+    SDL_QueryTexture(canvas, NULL, NULL, &canvas_w, &canvas_h);
+
+    int isize = char_tex_h / canvas_h;
+
+    SDL_Rect fontrect;
+    SDL_Rect canvrect;
+
+    fontrect.w = char_w;
+    fontrect.h = char_h;
+    fontrect.y = 0;
+
+    canvrect.w = char_w / isize;
+    canvrect.h = char_h / isize;
+    canvrect.y = 0;
+
+    SDL_SetRenderTarget(renderer, canvas);
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
+    SDL_RenderClear(renderer);
+    for(int i = 0; i<strlen(text); i++){
+        fontrect.x = font_map[text[i]] * char_w;
+        canvrect.x = i * char_w / isize;
+        SDL_RenderCopy(renderer, font_texture, &fontrect, &canvrect);
     }
+    SDL_SetRenderTarget(renderer, NULL);
 }
 
 void draw_pixel(SDL_Renderer* renderer, SDL_Texture* canvas, SDL_Color* color, int x, int y){
@@ -99,14 +109,43 @@ int main(){
     SDL_RenderFillRect(renderer, NULL);
     SDL_SetRenderTarget(renderer, NULL);
 
+    SDL_Texture* pi_text_canvas = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, 600, 40);
+    SDL_Texture* iter_text_canvas = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, 600, 40);
+    SDL_Rect pi_text_rect;
+    pi_text_rect.x = 0;
+    pi_text_rect.y = 0;
+    SDL_QueryTexture(pi_text_canvas, NULL, NULL, &(pi_text_rect.w), &(pi_text_rect.h));
+
+    SDL_Rect iter_text_rect;
+    iter_text_rect.x = 0;
+    iter_text_rect.y = 60;
+    SDL_QueryTexture(iter_text_canvas, NULL, NULL, &(iter_text_rect.w), &(iter_text_rect.h));
+
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    SDL_SetTextureBlendMode(pi_text_canvas, SDL_BLENDMODE_BLEND);
+    SDL_SetTextureBlendMode(iter_text_canvas, SDL_BLENDMODE_BLEND);
+
     int64_t insideCount = 0;
     int64_t outsideCount = 0;
 
-    SDL_Texture * char_texture;
-    if(create_char_texture(renderer, "digits.bmp", &char_texture) < 0){
-        fprintf(stderr, "create_char_texture failed: %s\n", SDL_GetError());
+    SDL_Texture * font_texture;
+    if(create_font_texture(renderer, "digits.bmp", &font_texture) < 0){
+        fprintf(stderr, "create_font_texture failed: %s\n", SDL_GetError());
         return 1;
     }
+
+    int fontmap[255];
+    fontmap['0'] = 0;
+    fontmap['1'] = 1;
+    fontmap['2'] = 2;
+    fontmap['3'] = 3;
+    fontmap['4'] = 4;
+    fontmap['5'] = 5;
+    fontmap['6'] = 6;
+    fontmap['7'] = 7;
+    fontmap['8'] = 8;
+    fontmap['9'] = 9;
+    fontmap['.'] = 10;
 
     int frame_counter = 0;
     int frame_counter_max = 5000;
@@ -156,18 +195,17 @@ int main(){
         SDL_RenderCopy(renderer, canvas, NULL, NULL);
     
         char pi_chars[30];
-        sprintf(pi_chars, "%.15lf", pi);
-        int pi_chars_indices[30];
-        for(int i = 0; i<strlen(pi_chars); i++)
-            pi_chars_indices[i] = pi_chars[i] - '0';
-        draw_chars(renderer, char_texture, pi_chars_indices, strlen(pi_chars), 0, 0, 8);
-
         char iter_chars[30];
+        sprintf(pi_chars, "%.15lf", pi);
         sprintf(iter_chars, "%ld", insideCount+outsideCount);
-        int iter_chars_indices[30];
-        for(int i = 0; i<strlen(iter_chars); i++)
-            iter_chars_indices[i] = iter_chars[i] - '0';
-        draw_chars(renderer, char_texture, iter_chars_indices, strlen(iter_chars), 0, 50, 8);
+
+        if(frame_counter % 200 == 0)
+            print_text(renderer, font_texture, 11, pi_text_canvas, fontmap, pi_chars);
+        SDL_RenderCopy(renderer, pi_text_canvas, NULL, &pi_text_rect);
+
+        if(frame_counter % 200 == 0)
+            print_text(renderer, font_texture, 11, iter_text_canvas, fontmap, iter_chars);
+        SDL_RenderCopy(renderer, iter_text_canvas, NULL, &iter_text_rect);
 
         SDL_RenderPresent(renderer);
         SDL_UpdateWindowSurface(window);
